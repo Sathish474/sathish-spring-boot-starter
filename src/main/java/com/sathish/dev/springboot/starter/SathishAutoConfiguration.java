46bb3040ac841ba46e6768406464d420d374726c
Sathish Nalabothula <sathish99@Sathishs-MBP.attlocal.net>
2023-03-21 22:06:51 -0500

    initial commit

---
+++ src/main/java/com/sathish/dev/springboot/starter/SathishAutoConfiguration.java
@@ -0,0 +1,33 @@
+package com.sathish.dev.springboot.starter;
+
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.vibur.dbcp.ViburDBCPDataSource;
+import org.vibur.dbcp.ViburDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+@ConditionalOnClass(ViburDataSource.class)
+@ConditionalOnMissingBean(DataSource.class)
+@AutoConfigureBefore(DataSourceAutoConfiguration.class)
+@EnableConfigurationProperties(SathishDataSourceProperties.class)
+public class SathishAutoConfiguration {
+
+    @Bean
+    public ViburDBCPDataSource dataSource(SathishDataSourceProperties properties) {
+        ViburDBCPDataSource ds = new ViburDBCPDataSource();
+        ds.setJdbcUrl(properties.getUrl());
+        ds.setUsername(properties.getUsername());
+        ds.setPassword(properties.getPassword());
+        ds.setDriverClassName(properties.getDriverClassName());
+        ds.start();
+        return ds;
+    }
+}
+
